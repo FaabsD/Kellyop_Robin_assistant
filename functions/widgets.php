3395874263c001f03dad578f90866c99c5772679
FaabsD <fabian.et.je@hotmail.com>
2022-03-10 19:01:40 +0100

    created widget areas for the footer

--- functions/widgets.php
+++ functions/widgets.php
@@ -14,4 +14,35 @@ function register_widgets()
         'before_title'  => '<h2>',
         'after_title'   => '</h2>',
     ));
+
+//    Register multiple footer widgets
+    $footer_widgets = array(
+        array(
+            'name'          => 'Footer content 1',
+            'id'            => 'footer-content-1',
+            'before_widget' => '<div class="footer-content footer-content-1">',
+            'after_widget'  => '</div>',
+            'before_title'  => '<h2>',
+            'after_title'   => '</h2>',
+        ),
+        array(
+            'name'          => 'Footer content 2',
+            'id'            => 'footer-content-2',
+            'before_widget' => '<div class="footer-content footer-content-2">',
+            'after_widget'  => '</div>',
+            'before_title'  => '<h2>',
+            'after_title'   => '</h2>',
+        ),
+        array(
+            'name'          => 'Footer content 3',
+            'id'            => 'footer-content-3',
+            'before_widget' => '<div class="footer-content footer-content-3">',
+            'after_widget'  => '</div>',
+            'before_title'  => '<h2>',
+            'after_title'   => '</h2>',
+        ),
+    );
+    foreach ($footer_widgets as $args) {
+        register_sidebar($args);
+    }
 }
