8a13fb5563e6b98f8d82c0e6a0d16b2c079118cf
FaabsD <fabian.et.je@hotmail.com>
2022-03-10 19:02:48 +0100

    show footer widgets

--- footer.php
+++ footer.php
@@ -1,5 +1,9 @@
 <footer>
-
+    <?php if ( function_exists('dynamic_sidebar') ) : ?>
+        <?php for($i = 1; $i <= 3; $i++) : ?>
+            <?php dynamic_sidebar('footer-content-' . $i) ?>
+        <?php endfor; ?>
+    <?php endif; ?>
 </footer>
 <?php wp_footer(); ?>
 </body>
