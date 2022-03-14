ea48d30542bf10400b8b500e5c6c1898e6fead71
FaabsD <fabian.et.je@hotmail.com>
2022-03-14 21:51:45 +0100

    display new widgets

--- footer.php
+++ footer.php
@@ -1,8 +1,10 @@
 <footer>
     <?php if ( function_exists('dynamic_sidebar') ) : ?>
-        <?php for($i = 1; $i <= 3; $i++) : ?>
+        <?php dynamic_sidebar('cta-footer'); ?>
+        <?php for($i = 1; $i <= 4; $i++) : ?>
             <?php dynamic_sidebar('footer-content-' . $i) ?>
         <?php endfor; ?>
+        <?php dynamic_sidebar('copyright-footer') ?>
     <?php endif; ?>
 </footer>
 <?php wp_footer(); ?>
