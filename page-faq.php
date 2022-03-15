bcee8e9d2a239678967a2c420bab981262b1657e
FaabsD <fabian.et.je@hotmail.com>
2022-03-15 15:35:11 +0100

    created an FAQ page template with styling (to continue)

---
+++ page-faq.php
@@ -0,0 +1,41 @@
+<?php
+/* Template Name: FAQ */
+$args = array('post_type' => 'FAQ');
+$query = new WP_Query($args);
+?>
+<?php get_header(); ?>
+<div class="site-content">
+    <div class="faq-container">
+        <div class="faq-container__items">
+            <?php if ( $query->have_posts() ) :
+            while ( $query->have_posts() ) :$query->the_post(); ?>
+                <div class="item">
+                    <div class="item__icon">
+                        <div class="icon icon-1">
+                            <img src="<?php echo THEME_URL . '/resources/icons/plus.svg' ?>" alt="">
+                        </div>
+                        <div class=" icon icon-2 icon--hidden">
+                            <img src="<?php echo THEME_URL . '/resources/icons/min.svg' ?>" alt="">
+                        </div>
+                    </div>
+                    <h3 class="item__title">
+                        <?php the_title(); ?>
+                    </h3>
+                    <div class="item__answer">
+                        <?php the_content(); ?>
+                    </div>
+                </div>
+            <?php endwhile; ?>
+        </div>
+        <?php else : ?>
+            <p>No FAQ's found</p>
+
+        <?php endif; ?>
+    </div>
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+        <div class="content__wrapper">
+            <?php the_content(); ?>
+        </div>
+    <?php endwhile; endif; ?>
+</div>
+<?php get_footer(); ?>
