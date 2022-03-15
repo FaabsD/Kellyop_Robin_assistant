f50832e7f3c5ccb4c4de749818672db4b4ddbc54
FaabsD <fabian.et.je@hotmail.com>
2022-03-15 15:35:42 +0100

    Created FAQ posttype

--- functions/post-types.php
+++ functions/post-types.php
@@ -6,27 +6,27 @@
 add_action('init', 'register_posttypes');
 
 function register_posttypes() {
-    /*register_post_type( '', array(
+    register_post_type( 'FAQ', array(
         'labels' => array(
-            'name'          => __( '', THEME_TEXT_DOMAIN ),
-            'singular_name' => __( '', THEME_TEXT_DOMAIN ),
-            'add_new' => __('Add ', THEME_TEXT_DOMAIN),
-            'add_new_item' => __('Add new ', THEME_TEXT_DOMAIN),
-            'all_items' => __('All ', THEME_TEXT_DOMAIN),
-            'edit_item' => __('Edit ', THEME_TEXT_DOMAIN),
-            'name_admin_bar' => __('', THEME_TEXT_DOMAIN),
-            'menu_name' => __('', THEME_TEXT_DOMAIN),
-            'new_item' => __('New ', THEME_TEXT_DOMAIN),
-            'not_found' => __('No  found', THEME_TEXT_DOMAIN),
-            'not_found_in_trash' => __('No  found in trash', THEME_TEXT_DOMAIN),
-            'search_items' => __('Search ', THEME_TEXT_DOMAIN),
-            'view_item' => __('View ', THEME_TEXT_DOMAIN),
+            'name'          => __( 'FAQ', THEME_TEXT_DOMAIN ),
+            'singular_name' => __( 'FAQ', THEME_TEXT_DOMAIN ),
+            'add_new' => __('Add FAQ', THEME_TEXT_DOMAIN),
+            'add_new_item' => __('Add new FAQ', THEME_TEXT_DOMAIN),
+            'all_items' => __("All FAQ's", THEME_TEXT_DOMAIN),
+            'edit_item' => __('Edit FAQ', THEME_TEXT_DOMAIN),
+            'name_admin_bar' => __('FAQ', THEME_TEXT_DOMAIN),
+            'menu_name' => __('FAQ', THEME_TEXT_DOMAIN),
+            'new_item' => __('New FAQ', THEME_TEXT_DOMAIN),
+            'not_found' => __("No FAQ's found", THEME_TEXT_DOMAIN),
+            'not_found_in_trash' => __("No FAQ's found in trash", THEME_TEXT_DOMAIN),
+            'search_items' => __('Search FAQ', THEME_TEXT_DOMAIN),
+            'view_item' => __('View FAQ', THEME_TEXT_DOMAIN),
         ),
         'public' => true,
         'has_archive' => true,
-        'menu_icon' => '',
+        'menu_icon' => 'dashicons-welcome-learn-more',
         'rewrite' => array('with_front' => true),
-        'supports' => array(),
+        'supports' => array('title', 'editor'),
         'show_in_rest' => false,
-    ) );*/
+    ) );
 }
